6df30ffeec9f5960fa322d2217b3d25c7378f08c
Rodrigo Rueda <rodrigo21492@gmail.com>
2026-03-14 19:56:25 -0300

    agregue verificaciones con un while para que sea numerico,positivo y no mayor a 50

--- js/ejercicio-07.js
+++ js/ejercicio-07.js
@@ -1,9 +1,14 @@
 //7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50)
 
-const numero = parseInt(prompt("Ingresa un numero no mayor a 50"));
+let num = prompt("Ingresa un numero no mayor a 50");
 const texto = document.getElementById("pantalla");
 
-if (numero <= 50 && numero>0) {
+while (isNaN(num) || num < 0 || num > 50) {
+  num = prompt("Dato incorrecto. Ingresa un numero no mayor a 50 y mayor a 0.");
+}
+
+if (num !== null) {
+  const numero = parseInt(num);
   for (let i = numero; i > 0; i--) {
     texto.innerHTML += `<p id="${i}"></p>`;
     const decrece = document.getElementById(`${i}`);
@@ -11,6 +16,4 @@ if (numero <= 50 && numero>0) {
       decrece.innerHTML += `${i}`;
     }
   }
-} else {
-  alert("dato incorrecto");
 }
