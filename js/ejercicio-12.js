c31034e03166eca9bc6ad7476481b02b6564353a
Rodrigo Rueda <rodrigo21492@gmail.com>
2026-03-15 16:51:24 -0300

    listo ejercicio 12

---
+++ js/ejercicio-12.js
@@ -0,0 +1,6 @@
+//12- Realiza un script que genere un número aleatorio entre 1 y 99
+
+const aleatorio = Math.floor(Math.random()*100)
+console.log(aleatorio)
+
+alert(`El numero generado es ${aleatorio}`)
